d20ca4851eebbcd5661e014d169ad5d4f8479370
Tadiwanashe Songore <119779084+TADI-I@users.noreply.github.com>
2025-08-16 11:18:58 +0200

    main setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDB2A99ojqoUlLjD-tBJLx1vF320CfTzmw",
+  authDomain: "subscription-management-65360.firebaseapp.com",
+  projectId: "subscription-management-65360",
+  storageBucket: "subscription-management-65360.firebasestorage.app",
+  messagingSenderId: "461111495497",
+  appId: "1:461111495497:web:de6665464e1ebad5ae2898"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
