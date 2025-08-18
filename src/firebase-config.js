19e89c63b2617f7d0d7e8379ceaa97608137694b
Tadiwanashe Songore <119779084+TADI-I@users.noreply.github.com>
2025-08-18 20:58:06 +0200

    .env for keys

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -9,18 +9,16 @@ import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDB2A99ojqoUlLjD-tBJLx1vF320CfTzmw",
-  authDomain: "subscription-management-65360.firebaseapp.com",
-  projectId: "subscription-management-65360",
-  storageBucket: "subscription-management-65360.firebasestorage.app",
-  messagingSenderId: "461111495497",
-  appId: "1:461111495497:web:de6665464e1ebad5ae2898"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const serverTimestamp = () => firebase.firestore.FieldValue.serverTimestamp();
 export const googleProvider = new GoogleAuthProvider();
 
